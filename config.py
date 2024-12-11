f21b9d9dd59e6379578dafd67c40462670ef260d
mikrostiff <realhouyigmail.com>
2024-12-11 01:36:54 +0000

    change

--- config.py
+++ config.py
@@ -10,12 +10,6 @@ else:
 # Set data directory in program folder
 DATA_DIR = os.path.join(APP_DIR, 'data')
 
-# # Create data directory if it doesn't exist
-# DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
-# if not os.path.exists(DATA_DIR):
-#     os.makedirs(DATA_DIR)
-
-# Global file paths
 # Global file paths
 USER_DATA_PATH = os.path.join(DATA_DIR, 'user_data.csv')
 MOOD_DATA_PATH = os.path.join(DATA_DIR, 'mood_data.csv')
