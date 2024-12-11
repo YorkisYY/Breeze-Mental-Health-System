9449be269ac6332a3fcd9218da0607f73ec425a8
mikrostiff <realhouyigmail.com>
2024-12-11 01:47:42 +0000

    packaging. run build_exe.py to build an exe file inside /dist folder

--- config.py
+++ config.py
@@ -3,12 +3,14 @@ import sys
 
 # Get application directory
 if getattr(sys, 'frozen', False):
-    APP_DIR = sys._MEIPASS
+    APP_DIR = os.path.dirname(sys.executable)
 else:
     APP_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Set data directory in program folder
 DATA_DIR = os.path.join(APP_DIR, 'data')
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
 
 # Global file paths
 USER_DATA_PATH = os.path.join(DATA_DIR, 'user_data.csv')
